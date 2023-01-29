e279865f547a859a4b50fce7353f790c6db91518
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2023-01-28 21:24:39 -0600

    hero partially done

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 
 const container = document.getElementById('root');
@@ -9,7 +9,11 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={extendTheme({
+      fonts: {
+        heading: `Grand Hotel, 'Open Sans', sans-serif`, 
+      }
+    })}>
       <App />
     </ChakraProvider>
   </StrictMode>
