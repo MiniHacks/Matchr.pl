1eb939c51b422ba139a89621634c85d23451019d
Katya Borisova <c.b.borisova@gmail.com>
2023-01-29 00:19:18 -0600

    more buttons

--- frontend/src/component/Swipe.jsx
+++ frontend/src/component/Swipe.jsx
@@ -7,13 +7,13 @@ import {
   Heading, 
   IconButton
 } from '@chakra-ui/react';
-import {ArrowBackIcon, ChevronUpIcon, ChevronDownIcon, ArrowLeftIcon, ArrowRightIcon} from '@chakra-ui/icons';
 import { SiteThemes, SiteSizes } from '../util/global';
-import {FiChevronsUp, FiChevronsDown, FiChevronUp, FiChevronDown, FiChevronLeft} from 'react-icons/fi';
+import {FiChevronsUp, FiChevronsDown, FiChevronUp, FiChevronDown, FiChevronLeft, FiInfo} from 'react-icons/fi';
 import Profile from "../util/profile";
 
 
 function Swipe() {
+  const pinkcolor = '#F7DBD7';
   console.log(Profile.getID());
   console.log(Profile.getElection());
   const [card, setCard] = useState({
@@ -37,17 +37,18 @@ function Swipe() {
     <Flex flexDirection='column' justify="space-around" align='center'>
       <Heading fontSize={SiteSizes.heading}> match.pol </Heading>
       <Flex> 
-        <Box borderRadius= '20' w = '350px' h = '600px' bg="#F7DBD7" paddingTop='25px' paddingBottom='50px' paddingRight='25px' paddingLeft='25px' margin-right='25px'>
+        <Box borderRadius= '20' w = '350px' h = '700px' bg= {pinkcolor} paddingTop='25px' paddingBottom='50px' paddingRight='25px' paddingLeft='25px' margin-right='25px'>
           <Text align='center' fontSize = {SiteSizes.subheading}> {card.quote}</Text>
+          <IconButton position='absolute' bottom='100px' right='40px' bg= {pinkcolor} isRound='true' icon={<FiInfo size='lg'/>}/>
         </Box>
         
         </Flex>
-     <Flex flexDirection= 'row' > 
-        <IconButton size = 'lg' bg= {SiteThemes.backgroundColor} isRound='true' icon={<FiChevronLeft size='lg'/>}/>
-        <IconButton size = 'lg' bg= {SiteThemes.backgroundColor} isRound='true' icon={<FiChevronsDown size='lg'/>}/>
-        <IconButton size = 'lg' bg= {SiteThemes.backgroundColor} isRound='true' icon={<FiChevronDown size='lg'/>}/>
-        <IconButton size = 'lg' bg= {SiteThemes.backgroundColor} isRound='true' icon={<FiChevronUp size='lg'/>}/>
-        <IconButton size = 'lg' bg= {SiteThemes.backgroundColor} isRound='true' icon={<FiChevronsUp size='lg'/>}/>
+     <Flex margin='10px' padding='10px' flexDirection= 'row' > 
+        <IconButton bg= {SiteThemes.backgroundColor} isRound='true' icon={<FiChevronLeft size='lg'/>}/>
+        <IconButton bg= {SiteThemes.backgroundColor} isRound='true' icon={<FiChevronsDown size='lg'/>}/>
+        <IconButton bg= {SiteThemes.backgroundColor} isRound='true' icon={<FiChevronDown size='lg'/>}/>
+        <IconButton bg= {SiteThemes.backgroundColor} isRound='true' icon={<FiChevronUp size='lg'/>}/>
+        <IconButton bg= {SiteThemes.backgroundColor} isRound='true' icon={<FiChevronsUp size='lg'/>}/>
       </Flex>
     </Flex>
   );
