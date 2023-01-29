9a0b2d185285c578be2ddb7d352fdaf15fcf1d62
Katya Borisova <c.b.borisova@gmail.com>
2023-01-29 00:42:29 -0600

    info

--- frontend/src/component/Swipe.jsx
+++ frontend/src/component/Swipe.jsx
@@ -6,8 +6,10 @@ import Profile from "../util/profile";
 
 function Swipe() {
   const pinkcolor = '#F7DBD7';
+  const [onCard, setOnCard] = useState(true);
   console.log(Profile.getID());
   console.log(Profile.getElection());
+
   const [card, setCard] = useState({
     quote : 'First-trimester abortion is murder',
     word : 'greeting',
@@ -15,6 +17,9 @@ function Swipe() {
     link : "https://.....com"
   })
 
+  // const callSetOnCard = () => {
+  //   setOnCard(!onCard);
+  // };
   useEffect(() => {
     async function getQuote(){
       const response = await fetch('http://localhost:8000/nq');
@@ -29,38 +34,69 @@ function Swipe() {
     return <>Still loading..</>
   }
 
+  if(onCard){
+    return (
+      <Flex 
+        flex="1"
+        flexDirection='column' 
+        justify="space-around" 
+        align='center'
+        padding="1rem"
+        gap="1rem"
+      >
+        <Heading fontSize={SiteSizes.heading}> match.pol </Heading>
+        <Flex 
+          flexDir="column" 
+          flex="1"
+          padding="2rem"
+          borderRadius="2rem"
+          bg={SiteThemes.mainColor}
+        >
+          <Box flex="0.5" align="center" fontSize={SiteSizes.subheading}>
+            <Text> Your Thoughts? </Text>
+          </Box>
+          <Box flex="0.8" align="center" fontSize={SiteSizes.subheading}>
+            <Text>{card.quote}</Text>
+            <IconButton onClick={()=>setOnCard(!onCard)} position='absolute'bottom='100px' right='40px' bg={pinkcolor} isRound='true' icon={<FiInfo size={60} />}/>
+          </Box>
+        </Flex>
+        <Flex gap="1rem" justify="space-between" > 
+          <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronLeft size={50} />}/>
+          <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronsDown size={50} />}/>
+          <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronDown size={50} />}/>
+          <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronUp size={50} />}/>
+          <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronsUp size={50} />}/>
+        </Flex>
+      </Flex>
+    );
+  }
   return (
     <Flex 
-      flex="1"
-      flexDirection='column' 
-      justify="space-around" 
-      align='center'
-      padding="1rem"
-      gap="1rem"
+    flex="1"
+    flexDirection='column' 
+    justify="space-around" 
+    align='center'
+    padding="1rem"
+    gap="1rem"
+  >
+    <Flex 
+      flexDir="column" 
+      padding="2rem"
+      borderRadius="2rem"
+      bg={SiteThemes.mainColor}
     >
-      <Heading fontSize={SiteSizes.heading}> match.pol </Heading>
-      <Flex 
-        flexDir="column" 
-        flex="1"
-        padding="2rem"
-        borderRadius="2rem"
-        bg={SiteThemes.mainColor}
-      >
-        <Box flex="0.5" align="center" fontSize={SiteSizes.subheading}>
-          <Text> Your Thoughts? </Text>
-        </Box>
-        <Box flex="0.8" align="center" fontSize={SiteSizes.subheading}>
-          <Text>{card.quote}</Text>
-        </Box>
-      </Flex>
-      <Flex gap="1rem" justify="space-between" > 
-        <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronLeft size={50} />}/>
-        <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronsDown size={50} />}/>
-        <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronDown size={50} />}/>
-        <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronUp size={50} />}/>
-        <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronsUp size={50} />}/>
-      </Flex>
+      <Box flex="0.8" align="center" fontSize={SiteSizes.subheading}>
+        <Text>{card.quote}</Text>
+        <IconButton onClick={()=>setOnCard(!onCard)} position='absolute'bottom='100px' right='40px' bg={pinkcolor} isRound='true' icon={<FiInfo size={60} />}/>
+      </Box>
     </Flex>
+    <Flex gap="1rem" justify="space-between" > 
+      <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronsDown size={50} />}/>
+      <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronDown size={50} />}/>
+      <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronUp size={50} />}/>
+      <IconButton bg={SiteThemes.backgroundColor} isRound='true' icon={<FiChevronsUp size={50} />}/>
+    </Flex>
+  </Flex>
   );
 }
 
